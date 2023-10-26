8afbd5b5c9f3ea03c93f0984b80d5302376b42f5
pratik <pratik.lahane1999@gmail.com>
2023-10-26 17:20:26 +0530

    final Leaderboard Optimisation

--- controller/premium.js
+++ controller/premium.js
@@ -1,53 +1,101 @@
 const expense = require("../model/expense");
 const user = require("../model/user");
+const sequelize = require("../util/SDatabase");
 
-// exports.showleaderboard = (req, res) => {
-//   user.findAll().then((ans) => {
-//     let sum = 0;
-//     ans.forEach((data) => {
-//       expense.findAll({ where: { userId: data.id } }).then((abc) => {
-//         sum = sum + abc.amount;
-//       });
-//       let obj = { name: data.name, Total: sum };
-//       res.status(201).json({ name: data.name, Total: sum });
+// code is correct but not efficient way to write code
+// exports.showleaderboard = async (req, res) => {
+//     try {
+//       const users = await user.findAll({attributes:['id', 'name']});
+
+//       const results = [];
+
+//       for (const data of users) {
+//         const expenses = await expense.findAll({ where: { userId: data.id } });
+
+//         let sum = 0;
+//         expenses.forEach((abc) => {
+//           sum += abc.amount;
+//         });
+
+//         results.push({ name: data.name, Total: sum });
+//       }
+//       console.log("results",results);
+//       console.log(results[1].Total);
+
+//       for (let i = 0; i < results.length - 1; i++) {
+//         for (let j = 0; j < results.length - 1 - i; j++) {
+//           if (results[j].Total < results[j + 1].Total) {
+//             // Swap the elements
+//             const temp = results[j];
+//             results[j] = results[j + 1];
+//             results[j + 1] = temp;
+//           }
+//         }
+//       }
+//       console.log("after sorting result = ", results);
+
+//       res.status(201).json(results);
+//     } catch (error) {
+//       console.error(error);
+//       res.status(500).json({ error: 'An error occurred while fetching data' });
+//     }
+//   };
+
+// one step best from above
+// exports.showleaderboard = async (req, res) => {
+//   try {
+//    const users = await user.findAll({attributes:["name", "id"]});
+// const expenses = await expense.findAll({attributes:["userId", "amount"]});
+//     const userAggregatedExpenses = {};
+//     expenses.forEach((expense) => {
+//       if (userAggregatedExpenses[expense.userId]) {
+//         userAggregatedExpenses[expense.userId] =
+//           userAggregatedExpenses[expense.userId] + expense.amount;
+//       } else {
+//         userAggregatedExpenses[expense.userId] = expense.amount;
+//       }
 //     });
-//   });
+//     console.log("object = ", userAggregatedExpenses);
+//     var userLeadershipBoardDetails = [];
+//     users.forEach((user) => {
+//       userLeadershipBoardDetails.push({
+//         name: user.name,
+//         total_cost: userAggregatedExpenses[user.id],
+//       });
+//     });
+//     userLeadershipBoardDetails.sort((a, b) => b.total_cost - a.total_cost);
+//     res.status(200).json(userLeadershipBoardDetails);
+//     console.log(userLeadershipBoardDetails);
+//   } catch (error) {
+//     console.error(error);
+//     res.status(500).json({ error: "An error occurred while fetching data" });
+//   }
 // };
 
+// two step best from above
+
 exports.showleaderboard = async (req, res) => {
-    try {
-      const users = await user.findAll();
-  
-      const results = [];
-  
-      for (const data of users) {
-        const expenses = await expense.findAll({ where: { userId: data.id } });
-  
-        let sum = 0;
-        expenses.forEach((abc) => {
-          sum += abc.amount;
-        });
-  
-        results.push({ name: data.name, Total: sum });
-      }
-      console.log("results",results);
-      console.log(results[1].Total);
+  try {
+    const userAggregatedExpenses = await user.findAll({
+      attributes: [
+        "name",
+        "id",
+        [sequelize.fn("sum", sequelize.col("amount")), "total_cost"],
+      ],
+      include: [
+        {
+          model: expense,
+          attributes: [],
+        },
+      ],
+      group: ["id"],
+      order: [[sequelize.col("total_cost"), "DESC"]],
+    });
 
-      for(let i = 0;i<results.length-1;i++) {
-        if((results[i].Total)<(results[i+1].Total)) {
-            let temp = results[i];
-            results[i] = results[i+1];
-            results[i+1] = temp;
-
-        }
-
-      }
-      console.log("after sorting result = ", results);
-  
-      res.status(201).json(results);
-    } catch (error) {
-      console.error(error);
-      res.status(500).json({ error: 'An error occurred while fetching data' });
-    }
-  };
-  
+    res.status(200).json(userAggregatedExpenses);
+    console.log(userAggregatedExpenses);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: "An error occurred while fetching data" });
+  }
+};
--- view/expense.js
+++ view/expense.js
@@ -47,10 +47,10 @@ function showAllExpenses() {
       headers: { Authorization: token },
     })
     .then((response) => {
-        const h1 = document.createElement("h1");
-        h1.textContent = "Expenses";
-        mainlist.appendChild(h1);
-        const ans = response.data.userData.forEach((data) => {
+      const h1 = document.createElement("h1");
+      h1.textContent = "Expenses";
+      mainlist.appendChild(h1);
+      const ans = response.data.userData.forEach((data) => {
         const { amount, description, category, id } = data;
         console.log("id, ", id);
         const li = document.createElement("li");
@@ -114,7 +114,7 @@ if (!Decode.premium) {
           handler: async function (response) {
             if (response.razorpay_payment_id) {
               // Payment succeeded
-             const Data =  await axios.post(
+              const Data = await axios.post(
                 "http://localhost:5000/purchase/updatetransactionstatus",
                 {
                   order_id: options.order_id,
@@ -154,30 +154,48 @@ if (!Decode.premium) {
 }
 
 const leadershipbtn = document.getElementById("leader-btn");
-leadershipbtn.addEventListener("click",(event) =>{
+// leadershipbtn.addEventListener("click",(event) =>{
+//   event.preventDefault();
+//   const list = document.getElementById("list");
+//   axios.get("http://localhost:5000/premium/showleaderboard", {
+//     headers: { Authorization: token },
+//   }).then((response)=>{
+//     list.textContent = " ";
+//     console.log(response.data);
+//     const h1 = document.createElement("h1");
+//   h1.textContent = "Leadership Board";
+//   list.appendChild(h1);
+//     for(let i = 0;i<response.data.length;i++) {
+//       console.log(response.data[i]);
+//       const li = document.createElement("li");
+//       li.textContent = `Name: ${response.data[i].name}, Total Expenses = ${response.data[i].Total}`;
+//       list.appendChild(li);
+//     }
+//   })
+
+// });
+
+leadershipbtn.addEventListener("click", (event) => {
   event.preventDefault();
   const list = document.getElementById("list");
-  axios.get("http://localhost:5000/premium/showleaderboard", {
-    headers: { Authorization: token },
-  }).then((response)=>{
-    list.textContent = " ";
-    console.log(response.data);
-    const h1 = document.createElement("h1");
-  h1.textContent = "Leadership Board";
-  list.appendChild(h1);
-    for(let i = 0;i<response.data.length;i++) {
-      console.log(response.data[i]);
-      const li = document.createElement("li");
-      li.textContent = `Name: ${response.data[i].name}, Total Expenses = ${response.data[i].Total}`;
-      list.appendChild(li);
-    }
-  })
-
+  axios
+    .get("http://localhost:5000/premium/showleaderboard", {
+      headers: { Authorization: token },
+    })
+    .then((response) => {
+      console.log(response.data);
+      list.textContent = " ";
+      const h1 = document.createElement("h1");
+      h1.textContent = "Leadership Board";
+      list.appendChild(h1);
+      for (let i = 0; i < response.data.length; i++) {
+        console.log(response.data[i]);
+        const li = document.createElement("li");
+        li.textContent = `Name: ${response.data[i].name}, Total Expenses = ${response.data[i].total_cost}`;
+        list.appendChild(li);
+      }
+    });
 });
-
-
-
-
 
 // tring to solve after refreshing if block below code execute but it is not executing
 // async function showAllExpenses() {
